9ae978913066bd43e04d00018d72b9a658f0e29b
FireRat <27763388+FireRat666@users.noreply.github.com>
2024-07-14 19:47:07 +1000

    test: update firescreen.js

--- test/firescreen.js
+++ test/firescreen.js
@@ -464,54 +464,109 @@ console.log("keepsoundlevel");
 ////////////////////////////////////////////////////////////////
 
 
-function tmutefunction(){
-		console.log("Mute Hand Clicked");
-};
-// Mute/UnMute Button
-function addfirescreenpart22() {
 
-    const handbutcontainer = document.createElement("a-entity");
-    handbutcontainer.setAttribute("scale", "0.1 0.1 0.1");
-    handbutcontainer.setAttribute("position", "0 0 0");
-    handbutcontainer.setAttribute("sq-lefthand", "whoToShow: onlyme;");
-    [
-      {
-        image: "https://firer.at/files/VolumeMute.png",
-        position: "-0.1 0.0 -0.0", 
-        buttontype: "fires-handmute", 
-        aattribute: "mute-hand", 
-        callback: () => this.tmutefunction()
-      },
-      {
-        image: "https://firer.at/files/VolumeHigh.png",
-        position: "-0.1 0.5 -0.5", 
-        buttontype: "fires-handcontrols", 
-        aattribute: "mute-hand", 
-        callback: () => this.tmutefunction()
-      }
-    ].forEach(item => {
-  let firesp22 = document.createElement("a-plane");
-  firesp22.setAttribute("sq-lefthand", "whoToShow: onlyme;");
-  firesp22.id = item.buttontype;
-  firesp22.setAttribute("position", item.position);
-  firesp22.setAttribute("scale", "0.5 0.5 0.5");
-  firesp22.setAttribute("rotation", "0 -90 180");
-  // firesp22.setAttribute("color", "#FFFFFF");
-  // firesp22.setAttribute("transparent", true);
-  // firesp22.setAttribute("material", "transparent: true");
-  firesp22.setAttribute("sq-collider");
-  firesp22.setAttribute("sq-interactable");
-  firesp22.setAttribute("class", "buttons");
-  firesp22.setAttribute("src", item.image);
-  firesp22.setAttribute(item.aattribute, "");
-  firesp22.addEventListener("click", () => item.callback());
-  // firescreen.appendChild(firesp22);
-  // document.querySelector("a-scene").appendChild(firesp22);
-  handbutcontainer.appendChild(firesp22);
-  console.log("Add Hand Button Test");
-      })
-  document.querySelector("a-scene").appendChild(handbutcontainer);
+
+class handButtonCrap{
+	constructor() {
+	  console.log("Hand Button Constructor Loading.");
+	  if(window.isBanter) { 
+		setTimeout(() => { 
+		  this.setupHandControls();
+		}, 11000); 
+	  };
+	}
+
+	mute() {
+		if (handbuttonmutestate) {
+		handbuttonmutestate = false;
+		console.log("handbuttonmutestate Set to False");
+		} else {
+		handbuttonmutestate = true;
+		console.log("handbuttonmutestate Set to True");
+		};
+		document.querySelectorAll('.firescreenc')
+		.forEach((firescreenc) => {
+			if(handbuttonmutestate) {
+			firescreenc.components["sq-browser"].runActions([ { actionType: "runscript", strparam1:
+			"document.querySelectorAll('video, audio').forEach((elem) => elem.muted=false); ", }, ]);
+			console.log("Browser Muted Set to False")
+			} else {
+			firescreenc.components["sq-browser"].runActions([ { actionType: "runscript", strparam1:
+			"document.querySelectorAll('video, audio').forEach((elem) => elem.muted=true); ", }, ]);
+			console.log("Browser Muted Set to True")
+			}
+		});
+	console.log("test mute button clicked")
+	}
+
+
+	volumecontrol(vvalue) {
+		document.querySelectorAll('.firescreenc')
+		.forEach((firescreenc) => {
+			let volume = parseFloat(firescreenc.getAttribute("volumelevel"));
+			volume += parseFloat(vvalue);
+			volume = volume.toFixed(2);
+			console.log("Volume is: " + volume)
+			if (volume > 1) {volume = 1};
+			if (volume < 0) {volume = 0};
+			firescreenc.setAttribute("volumelevel", volume);
+			firescreenc.components["sq-browser"].runActions([ { actionType: "runscript", strparam1:
+			"document.querySelectorAll('video, audio').forEach((elem) => elem.volume=" + volume + ");", }, ]);
+		});
+	console.log("testbut button clicked")
+	}
+
+
+	setupHandControls() {
+		console.log("Setting up Test Hand Controls")
+		// This was a great innovation by HBR, who wanted Skizot to also get credit for the original idea. 
+		const handControlsContainer = document.createElement("a-entity");
+		handControlsContainer.setAttribute("scale", "0.08 0.08 0.08");
+		handControlsContainer.setAttribute("position", "0.02 0.006 -0.010");
+		handControlsContainer.setAttribute("sq-lefthand", "whoToShow: " + user.id);
+		[
+			{
+			image: "https://firer.at/files/VolumeHigh.png",
+			position: "-1 0.2 -0.4", 
+			testthing: "0.05", 
+			colour: "#00FF00", 
+			callback: () => this.volumecontrol("0.05")
+			},
+			{
+			image: "https://firer.at/files/VolumeLow.png",
+			position: "-1 0.2 0", 
+			testthing: "-0.05", 
+			colour: "#00FF00", 
+			callback: () => this.volumecontrol("-0.05")
+			},
+			{
+			image: "https://firer.at/files/VolumeMute.png",
+			position: "-1 0.2 0.4", 
+			testthing: "-0.05", 
+			colour: "#00FF00", 
+			callback: () => this.mute()
+			}
+		].forEach(item => {
+			const button = document.createElement("a-plane");
+			button.setAttribute("sq-interactable", "");
+			button.setAttribute("sq-collider", "");
+			button.setAttribute("scale", "0.4 0.4 0.4");
+			button.setAttribute("rotation", "0 -90 180");
+			button.setAttribute("src", item.image);
+			button.setAttribute("color", item.colour);
+			button.setAttribute("transparent", true);
+			button.setAttribute("position", item.position);
+			button.setAttribute("handthing", item.testthing);
+			button.addEventListener("click", () => item.callback());
+			handControlsContainer.appendChild(button);
+		})
+		document.querySelector("a-scene").appendChild(handControlsContainer);
+	}
+
 };
+
+let handbuttonmutestate = false;
+const handbuttonstuff = new handButtonCrap();
 
 
 ////////////////////////////////////////////////////////////////
@@ -627,7 +682,7 @@ function addfirescreenpart22() {
 		setTimeout(() => {  this.el.setAttribute("color", buttoncolor); }, 100); 
 		});        },      });
 
-	// Toggle for hiding and showing the scale buttons By Fire with help from HBR
+	// Toggle for hiding and showing the rotation buttons By Fire with help from HBR
   AFRAME.registerComponent("enablerot", {
 	init: function () {
 	  this.el.addEventListener("click", () => {
