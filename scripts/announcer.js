7b3165e3c807ce871dab13e3b55e3854a5351288
FireRat <27763388+FireRat666@users.noreply.github.com>
2024-07-25 18:03:22 +1000

    update announcer.js

--- scripts/announcer.js
+++ scripts/announcer.js
@@ -15,16 +15,39 @@ async function speak(text) {
 if(window.isBanter) {
   const now = Date.now();
   window.userJoinedCallback = async user => {
-    if(Date.now() - now > 20000) {
+    if(Date.now() - now > 15000) {
       username = (user.name ? user.name : user.id.substr(0, 6));
+      theusersid = user.id
+      if (username === "Gravxton") {username = "Graviton Fucking Hell "}
+      if (username === "Vanquish3r") {username = "Vanquisher"}
       const welcomeMessages = [
         username + " welcome message blah blah!",
+        username + " Joined your party",
+        username + " has spawned into reality",
+        username + " just showed up, Hold my Head Set",
+        username + " just showed up, Don't let them leave",
+        username + " just showed up, Quick call the police",
+        username + " just showed up, Everyone act normal",
         username + ", What the hell, you broke everything, it was just working, what did you do?!",
         username + " has joined, what will they do now?",
+        username + " might be an alien, watch them carefully",
+        username + " just stumbled over their charger",
+        "Oh No! " + username + " has spawned into reality",
+        "Oh No! " + username + " Left their toilet seat up",
+        "Oh No! " + username + " is about to run out of battery",
+        "Oh No! " + username + " Has forgotten their own name",
+        "Oh No! " + username + " is running out of gas",
+        "Nobody tell " + username + " Their still in their pyjamas",
+        "Oh No! Hide your sheep " + username + " has joined the space",
+        "Oh No! " + username + " needs a recharge, grab the defib",
         "Your King " + username + " has joined the space.",
         "Your Queen " + username + " has joined the space.",
+        "Your Magesty " + username + " has joined the space.",
+        "The rumours are true " + username + " has joined the space",
         "Bow to your King " + username,
         "Bow to your Queen " + username,
+        "Here we go again, " + username + " has joined the space.",
+        username + " Has Joined the space, And wants to know if you are hungry girl",
         username + " was pushed into a portal, quick call the police",
         username + ", be careful of DedZed the fish overlord"
         ];
@@ -35,9 +58,12 @@ if(window.isBanter) {
       timevariable = hours + "" + minutes + "" + second;
       let psudorandomvar = PRNGF(timevariable, welcomeMessages.length);
       let randommessage = welcomeMessages[psudorandomvar];
+      // let randommessage = welcomeMessages[Math.floor(Math.random() * welcomeMessages.length)];
       const message = randommessage; 
       await speak(message);
-    };
+    } else {
+      console.log("ANNOUNCER: Early User Joined Callback event");
+    }
   }
 };
 
@@ -47,7 +73,7 @@ if(window.isBanter) {
     str = `${(2**31-1&Math.imul(48271,seed))/2**31}`
     .split('')
     .slice(-10)
-    .join('') % (modulo + 1) // Add one to the modulo to fix the issue
+    .join('') % (modulo) // Add one to the modulo to fix the issue
     return str
 }
 
@@ -96,6 +122,14 @@ function announcerloadtest() {
         username + ", What the hell, you broke everything, it was just working, what did you do? ",
         "Hello, Welcome to the space " + username,
         "What are you doing here " + username,
+        "Welcome to [Space Name]! We're never letting you go. Quick, lock the doors!",
+        "Welcome to [Space Name] Zoo! Please, don't feed the animals. ",
+        "Welcome aboard! " + username + " We’re so excited to have you with us",
+        "Welcome " + username + " we Hope you brought your sense of humor!",
+        "Glad you could join us " + username + " now let’s have some fun!",
+        "Fasten your seatbelt " + username + " it’s going to be a wild ride.",
+        "Hi there! It’s great to meet you " + username + " , wait a sec I am not sentient",
+        "Welcome " + username + " We’re a little weird, a little wacky, but we’re pretty sure you’ll fit right in.",
         "Welcome your Highness " + username,
         "Hello " + username + " " + username + " " + username + " " + username + " Failure detected, shutting down",
         "Enjoy your stay " + username
