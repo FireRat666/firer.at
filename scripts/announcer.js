624758680ebf9427f730828e068e087a93e0aeaa
FireRat <27763388+FireRat666@users.noreply.github.com>
2024-07-26 17:00:53 +1000

    update announcer.js

--- scripts/announcer.js
+++ scripts/announcer.js
@@ -67,7 +67,7 @@ if(window.isBanter && announceevents === true) {
 };
 
 const announcerscene = BS.BanterScene.getInstance();
-var now = 9999999999999;
+var now = 9999999999999; // Set Now to a Really Big Number, so if user-joined is called before unity-loaded, it wont spam user joined messages for users that were already in the space
 // Welcome message for user entering the space
 function announcerloadtest() {
   announcerscene.On("unity-loaded", () => {
@@ -151,7 +151,7 @@ function announcerloadtest() {
           ];
 
         let psudorandomvar = GETPRNGF(welcomeMessages.length);
-        const message = welcomeMessages[psudorandomvar]; 
+        const message = welcomeMessages[GETPRNGF(welcomeMessages.length)]; 
         speak(message);
         console.log("USERNAME: " + e.detail.name + " USERID: " + theusersid + " PRVAR: " + psudorandomvar);
       };
