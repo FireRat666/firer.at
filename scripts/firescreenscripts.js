232b613d333a9e12c3c629305052f96b035ec3a9
FireRat <27763388+FireRat666@users.noreply.github.com>
2025-05-10 15:23:56 +1000

    update firescreenscripts.js

--- scripts/firescreenscripts.js
+++ scripts/firescreenscripts.js
@@ -357,7 +357,7 @@ async function sdk2tests(p_pos, p_rot, p_sca, p_castmode, p_lockposition, p_scre
 
   firescenev2.addEventListener("user-already-joined", (e) => {
       console.log("User already joined:", e.detail);
-      if (p_handbuttons === "true" && e.detail.isLocal) { window.firstrunhandcontrols = false;
+      if (p_handbuttons === "true" && e.detail.isLocal) { window.firstrunhandcontrols = false; playersuseridv2 = e.detail.uid;
         console.log("FIRESCREEN2: Local User-already-joined, Enabling Hand Controls"); setupHandControlsV2();
         console.log(`window.firstrunhandcontrols:${window.firstrunhandcontrols}`);
       };
