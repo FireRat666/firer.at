84f15488178c74014d514430ebeb100638061b66
FireRat <27763388+FireRat666@users.noreply.github.com>
2026-02-25 21:02:24 +1100

    update announcer.js

--- scripts/announcer.js
+++ scripts/announcer.js
@@ -14,9 +14,27 @@ async function loadAndExecuteAnnouncerScript(src) {
     console.log(`Announcer Script executed successfully! YT Detected:${checkForMatchingAnnouncerScripts()}`);
   } catch (error) { console.error("Failed to load or execute the Announcer script:", error); }
 }
+function StartAnnouncer() {
+  if (!window.AnnouncerScriptInitialized) {
+    window.AnnouncerScriptInitialized = true;
+    loadAndExecuteAnnouncerScript(`https://firer.at/scripts/announcerscripts.js`);
+    console.log(`Announcer Script FIRST Call, Should be Loading!!`);
+  } else { console.log(`Announcer Script Already Called, Should be Loading!!`); }
+}
 
-if (!window.AnnouncerScriptInitialized) {
-  window.AnnouncerScriptInitialized = true;
-  loadAndExecuteAnnouncerScript(`https://firer.at/scripts/announcerscripts.js`);
-  console.log(`Announcer Script FIRST Call, Should be Loading!!`);
-} else { console.log(`Announcer Script Already Called, Should be Loading!!`); }
+function checkForBS() {
+  if (window.BS) {
+    // BS is loaded, so we can now execute the script
+    console.log(`Announcer Script BS is loaded, so we can now execute the script`);
+    StartAnnouncer();
+  } else {
+    // BS not loaded yet, wait for it
+    window.addEventListener("bs-loaded", () => {
+      console.log(`Announcer Script BS not loaded yet, wait for it`);
+      StartAnnouncer();
+    })
+  }
+    console.log(`Announcer Script Checked for BS`);
+}
+
+checkForBS();
